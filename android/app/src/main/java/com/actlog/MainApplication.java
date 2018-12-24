ac55d37148c7e66e93da028944d24a4c16286842
MrFermz <Mr.Fermz@hotmail.com>
2018-12-25 00:52:48 +0700

    from PC

--- android/app/src/main/java/com/actlog/MainApplication.java
+++ android/app/src/main/java/com/actlog/MainApplication.java
@@ -5,10 +5,15 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +31,9 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseAuthPackage()
       );
     }
 
--- src/RegisterScreen.js
+++ src/RegisterScreen.js
@@ -4,6 +4,7 @@ import {
   ScrollView,
   TouchableOpacity,
   Alert,
+  View,
 } from 'react-native';
 import {
   StackActions,
@@ -12,64 +13,48 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { Input, Avatar } from 'react-native-elements';
 import axios from 'axios'
+import firebase from 'react-native-firebase'
+// import firebase from 'firebase'
 import styles from './styles'
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBHwakqkRXJ94foc8_unxo-IiI0_eo_zsQ",
+  authDomain: "actlog-912c1.firebaseapp.com",
+  databaseURL: "https://actlog-912c1.firebaseio.com",
+  projectId: "actlog-912c1",
+  storageBucket: "actlog-912c1.appspot.com",
+  messagingSenderId: "550723713394"
+}
+firebase.initializeApp(config)
+
 class RegisterScreen extends Component {
+
   constructor(props) {
     super(props)
-    this.state = {
-      username: '',
+    this.state = ({
+      email: '',
       password: '',
-      userType: ''
-    }
+    })
   }
 
-  async onRegisterPressed() {
-    try {
-      const { username, password, userType } = this.state
-      const data = {
-        username: username,
-        password: password,
-        userType: userType
-      }
-
-      axios.post('http://localhost:8082/api/v1/register', data)
-        // axios.post('http://192.168.1.101:8082/api/v1/register', data)
-        .then((response) => {
-          const result = response.data.result
-          if (result == 'success') {
-            Alert.alert('Register Successful', '',
-              [
-                { text: 'OK', onPress: () => this.props.navigation.goBack() }
-              ])
-          } else {
-            Alert.alert('Register Failed')
-          }
-        })
-        .catch(function (error) {
-          console.log(error)
-          Alert.alert(JSON.stringify(error))
-        })
-    } catch (error) {
-    }
+  register = () => {
+    const { email, password } = this.state;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => { alert("Successful , " + email + " " + password); })
+      .catch((msgError) => { alert(msgError.message) })
   }
 
   render() {
     let icoSize = 30
     return (
-      <ScrollView
-        style={styles.common.scrollView}>
-        <Avatar
-          containerStyle={styles.detail.avatar}
-          rounded
-          size='xlarge'
-          source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg" }} />
+      <ScrollView style={styles.common.scrollView}>
         <Input
           inputStyle={styles.common.inputText}
           containerStyle={styles.common.input}
           inputContainerStyle={styles.common.input_}
           placeholderTextColor='white'
-          onChangeText={(text) => this.setState({ username: text })}
+          onChangeText={(text) => this.setState({ email: text })}
           autoCorrect={false}
           autoCapitalize={false}
           keyboardType={'email-address'}
@@ -80,7 +65,7 @@ class RegisterScreen extends Component {
               color='white'
             />
           }
-          placeholder='ชื่อผู้ใช้' />
+          placeholder={this.state.email} />
         <Input
           inputStyle={styles.common.inputText}
           containerStyle={styles.common.input}
@@ -97,15 +82,108 @@ class RegisterScreen extends Component {
               color='white'
             />
           }
-          placeholder='รหัสผ่าน' />
+          placeholder={this.state.password} />
         <TouchableOpacity
           style={styles.common.button}
-          onPress={this.onRegisterPressed.bind(this)}>
+          onPress={this.register.bind(this)}>
           <Text style={styles.common.buttonText}>สมัครสมาชิก</Text>
         </TouchableOpacity>
+        <Text>{this.state.email + this.state.password}</Text>
       </ScrollView>
-    );
+    )
   }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     username: '',
+  //     password: '',
+  //     userType: ''
+  //   }
+  // }
+
+  // async onRegisterPressed() {
+  //   try {
+  //     const { username, password, userType } = this.state
+  //     const data = {
+  //       username: username,
+  //       password: password,
+  //       userType: userType
+  //     }
+
+  //     axios.post('http://localhost:8082/api/v1/register', data)
+  //       // axios.post('http://192.168.1.101:8082/api/v1/register', data)
+  //       .then((response) => {
+  //         const result = response.data.result
+  //         if (result == 'success') {
+  //           Alert.alert('Register Successful', '',
+  //             [
+  //               { text: 'OK', onPress: () => this.props.navigation.goBack() }
+  //             ])
+  //         } else {
+  //           Alert.alert('Register Failed')
+  //         }
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error)
+  //         Alert.alert(JSON.stringify(error))
+  //       })
+  //   } catch (error) {
+  //   }
+  // }
+  //
+  // render() {
+  //   let icoSize = 30
+  //   return (
+  //     <ScrollView
+  //       style={styles.common.scrollView}>
+  //       <Avatar
+  //         containerStyle={styles.detail.avatar}
+  //         rounded
+  //         size='xlarge'
+  //         source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg" }} />
+  //       <Input
+  //         inputStyle={styles.common.inputText}
+  //         containerStyle={styles.common.input}
+  //         inputContainerStyle={styles.common.input_}
+  //         placeholderTextColor='white'
+  //         onChangeText={(text) => this.setState({ username: text })}
+  //         autoCorrect={false}
+  //         autoCapitalize={false}
+  //         keyboardType={'email-address'}
+  //         leftIcon={
+  //           <Icon
+  //             name='user-alt'
+  //             size={icoSize}
+  //             color='white'
+  //           />
+  //         }
+  //         placeholder='ชื่อผู้ใช้' />
+  //       <Input
+  //         inputStyle={styles.common.inputText}
+  //         containerStyle={styles.common.input}
+  //         inputContainerStyle={styles.common.input_}
+  //         placeholderTextColor='white'
+  //         onChangeText={(text) => this.setState({ password: text })}
+  //         autoCorrect={false}
+  //         autoCapitalize={false}
+  //         secureTextEntry={true}
+  //         leftIcon={
+  //           <Icon
+  //             name='lock'
+  //             size={icoSize}
+  //             color='white'
+  //           />
+  //         }
+  //         placeholder='รหัสผ่าน' />
+  //       <TouchableOpacity
+  //         style={styles.common.button}
+  //       // onPress={this.onRegisterPressed.bind(this)}
+  //       >
+  //         <Text style={styles.common.buttonText}>สมัครสมาชิก</Text>
+  //       </TouchableOpacity>
+  //     </ScrollView>
+  //   );
+  // }
 }
 
 export default RegisterScreen;
