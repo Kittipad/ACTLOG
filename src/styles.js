74d935f050e44f85f6555a07511e76c1ef196eee
Kate <mrfermz@Kate.local>
2019-02-24 18:34:19 +0700

    code clean

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native'
 
 const view = StyleSheet.create({
+  timeTableContainer: {
+    alignItems: 'flex-start'
+  },
   loginContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -77,7 +80,25 @@ const button = StyleSheet.create({
   subLabel: {
     color: 'black',
     fontSize: 20
-  }
+  },
+  timeButtonLeft: {
+    backgroundColor: '#34495E',
+    width: 120, height: 50,
+    justifyContent: 'center',
+    borderBottomLeftRadius: 10,
+    borderTopLeftRadius: 10,
+    marginLeft: 10,
+    marginRight: 1
+  },
+  timeButtonRight: {
+    backgroundColor: '#34495E',
+    width: 120, height: 50,
+    justifyContent: 'center',
+    borderBottomRightRadius: 10,
+    borderTopRightRadius: 10,
+    marginLeft: 1,
+    marginRight: 10
+  },
 })
 
 const input = StyleSheet.create({
@@ -106,6 +127,18 @@ const input = StyleSheet.create({
     fontSize: 20,
     paddingLeft: 10,
     paddingRight: 10
+  },
+  actField: {
+    width: '90%', height: 250,
+    marginBottom: 10,
+    marginTop: 15,
+    borderRadius: 5,
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    fontSize: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray'
   }
 })
 
@@ -137,9 +170,27 @@ const label = StyleSheet.create({
     fontSize: 30,
     margin: 10
   },
+  headerTimeTable: {
+    fontSize: 25,
+    marginBottom: 5,
+    marginLeft: 5,
+    alignSelf: 'center',
+    color: '#34495E'
+  },
   sub: {
     fontSize: 20,
     color: '#34495E'
+  },
+  _sub: {
+    alignSelf: 'center',
+    fontSize: 20,
+    color: 'white',
+    margin: 5
+  },
+  subAct: {
+    fontSize: 20,
+    color: '#34495E',
+    marginBottom: 20
   },
   detail: {
     fontSize: 20,
@@ -154,236 +205,16 @@ const label = StyleSheet.create({
     fontSize: 20,
     marginTop: 10,
     color: '#34495E'
-  }
-})
-
-const detail = StyleSheet.create({
-  detailContainer: {
-    flexDirection: 'row',
-    marginBottom: 5,
   },
-  detailLabelContainer: {
-    alignItems: 'flex-start'
-  },
-  label: {
-    fontSize: 20,
-    marginTop: 20,
-    marginBottom: 20,
-    marginLeft: 15,
-    width: 300,
-    alignSelf: 'center',
-    color: '#34495E'
-  },
-  name: {
-    fontSize: 30,
-    marginBottom: 20,
-    color: '#34495E',
-    alignSelf: 'center'
-  },
-  icon: {
-
-  },
-  input: {
-    width: '90%', height: 50,
+  actLabel: {
     marginBottom: 10,
-    marginTop: 15,
-    borderRadius: 5,
-    alignSelf: 'center',
-    backgroundColor: 'white',
     fontSize: 20,
-    padding: 10,
-  }
-})
-
-const common = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#5499C7',
-  },
-  input: {
-    width: "75%", height: 50,
-    margin: 10,
-    borderRadius: 5,
-    padding: 0,
-    alignSelf: 'center',
-  },
-  input_: {
-    borderBottomColor: 'white'
-  },
-  inputText: {
-    color: 'white'
-  },
-  scrollView: {
-    backgroundColor: '#5499C7',
-    width: '100%',
-  },
-  label: {
-    fontSize: 20,
-    color: 'white'
-  },
-  _label: {
-    fontSize: 20,
-  },
-  button: {
-    width: 200, height: 50,
-    borderWidth: 1,
-    borderColor: 'white',
-    backgroundColor: 'white',
-    marginTop: 15,
-    borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center'
-  },
-  _button: {
-    width: 200, height: 50,
-    borderWidth: 1,
-    borderColor: '#5499C7',
-    backgroundColor: '#5499C7',
-    marginTop: 15,
-    borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center'
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#5499C7',
-    fontWeight: 'bold'
-  },
-  _buttonText: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold'
-  },
-  listView: {
-    backgroundColor: '#5499C7',
-    borderRadius: 10,
-    height: 50,
-  },
-  listViewText: {
-    marginLeft: 10,
-    fontSize: 20
-  },
-  headerRight: {
-    marginRight: 10
-  },
-  card: {
-    alignItems: 'center',
-    borderRadius: 20,
-    width: '95%',
-  }
-})
-
-const home = StyleSheet.create({
-  container: {
-    flex: 1, flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    backgroundColor: '#5499C7'
-  },
-  menu: {
-    width: 150, height: 150,
-    marginTop: 30,
-    marginLeft: 15,
-    marginRight: 15,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  menuItem: {
     backgroundColor: 'lightgray',
-    fontSize: 20
-  }
-})
-
-const timeTable = StyleSheet.create({
-  container: {
-    alignItems: 'flex-start'
-  },
-  headerLabel: {
-    fontSize: 25,
-    marginBottom: 5,
-    marginLeft: 5,
-    alignSelf: 'center',
-    color: '#34495E'
-  },
-  timeLabel: {
-    alignSelf: 'center',
-    fontSize: 20,
-    color: 'white',
-    margin: 5
-  },
-  timeButtonLeft: {
-    backgroundColor: '#34495E',
-    width: 120, height: 50,
-    justifyContent: 'center',
-    borderBottomLeftRadius: 10,
-    borderTopLeftRadius: 10,
-    marginLeft: 10,
-    marginRight: 1
-  },
-  timeButtonRight: {
-    backgroundColor: '#34495E',
-    width: 120, height: 50,
-    justifyContent: 'center',
-    borderBottomRightRadius: 10,
-    borderTopRightRadius: 10,
-    marginRight: 10,
-    marginLeft: 1
-  }
-})
-
-const activity = StyleSheet.create({
-  container: {
-    alignItems: 'center'
-  },
-  label: {
-    marginBottom: 10,
-    fontSize: 20,
-    backgroundColor: '#34495E',
-    color: 'white',
-    paddingRight: 50,
-    paddingLeft: 50,
-    paddingTop: 5,
-    paddingBottom: 5
-  },
-  detail: {
-    marginBottom: 20,
-    fontSize: 20
-  },
-  input: {
-    width: '90%', height: 250,
-    marginBottom: 10,
-    marginTop: 15,
-    borderRadius: 5,
-    alignSelf: 'center',
-    backgroundColor: 'white',
-    fontSize: 20,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'gray'
-  }
-})
-
-const visit = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-  },
-  label: {
-    marginBottom: 10,
-    fontSize: 30,
     color: '#34495E',
     paddingRight: 50,
     paddingLeft: 50,
     paddingTop: 5,
     paddingBottom: 5
-  },
-  labelSub: {
-    fontSize: 20,
-    color: 'gray'
   }
 })
 
@@ -402,11 +233,5 @@ export default {
   input,
   icon,
   label,
-  common,
-  home,
-  detail,
-  timeTable,
-  activity,
-  visit,
   error
 }
